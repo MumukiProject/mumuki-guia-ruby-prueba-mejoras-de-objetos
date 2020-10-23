ea6e88eb66951a97e2a011ccd1654a0d83f05ee5
gustavo@mumuki.org <gustavo@mumuki.org>
2020-10-23 21:01:50 +0000

    Mumuki Export on 2020-10-23 21:01:50 +0000

--- 00003_4_ Voy a hacer, pero como yo quiero/test.rb
+++ 00003_4_ Voy a hacer, pero como yo quiero/test.rb
@@ -1,5 +1,29 @@
-describe 'Descripción del grupo' do
-  it 'Descripción del ejemplo' do
-    expect(true).to eq true
+module Pepita
+  def self.energia=(cuanto)
+    @energia = cuanto
   end
+end
+
+describe 'Pepita' do
+  it 'entiende hacer_lo_que_quiera!' do
+    expect(Pepita).to respond_to :hacer_lo_que_quiera!
+  end
+  
+  it 'cuando está débil y hace lo que quiere, come 10 gramos de alpiste' do
+    Pepita.energia = 80
+    Pepita.hacer_lo_que_quiera!
+    expect(Pepita.energia).to eq 230
+  end
+  
+  it 'cuando no está débil, pero sí feliz y hace lo que quiere, vuela en círculos cinco veces' do
+    Pepita.energia = 1010
+    Pepita.hacer_lo_que_quiera!
+    expect(Pepita.energia).to eq 960
+  end
+  
+  it 'cuando no está ni débil ni feliz y hace lo que quiere, come 5 gramos de alpiste' do
+    Pepita.energia = 500
+    Pepita.hacer_lo_que_quiera!
+    expect(Pepita.energia).to eq 575
+  end  
 end
