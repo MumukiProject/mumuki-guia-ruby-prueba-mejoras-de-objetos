e5442d5436b13727880bc7c0693ed511a5ef2fc1
gustavo@mumuki.org <gustavo@mumuki.org>
2021-02-24 17:57:49 +0000

    Mumuki Export on 2021-02-24 17:57:49 +0000

--- 00011_Para cerrar/test.rb
+++ 00011_Para cerrar/test.rb
@@ -8,14 +8,14 @@ end
 
 it "Los Fideos inicialmente no están picantes" do
   resultado = Fideos.picantes?
-  expect(resultado).to be(false), "Fideos.picantes? debería ser false porque inicialmente no tienen ajíes. Sin embargo, Fideo.picantes? fue #{resultado}"
+  expect(resultado).to be(false), "Fideos.picantes? debería ser false porque inicialmente no tienen ajíes. Sin embargo, Fideo.picantes? fue #{resultado}."
 end
 
 it "Si Jor picantea su plato, los Fideos quedan picantes" do
   Jor.plato_del_dia = Fideos
   Jor.picantear!
   resultado = Fideos.picantes?
-  expect(resultado).to be(true), "Fideos.picantes? debería ser true porque deberían tener 5 ajíes. Sin embargo, Fideos.picantes? fue #{resultado}"
+  expect(resultado).to be(true), "Fideos.picantes? debería ser true porque deberían tener 5 ajíes. Sin embargo, Fideos.picantes? fue #{resultado}."
 end
 
 it "Si Jor picantea su plato y Luchi luego suaviza en 2 ajíes, los Fideos quedan picantes" do
@@ -27,11 +27,14 @@ it "Si Jor picantea su plato y Luchi luego suaviza en 2 ajíes, los Fideos queda
 end
 
 it "Si Jor picantea su plato y Luchi luego suaviza en 3 ajíes, los Fideos no quedan picantes" do
-  Jor.plato_del_dia = Fideos
-  Jor.picantear!
-  Luchi.suavizar! Fideos, 3
-  resultado = Fideos.picantes?
-  expect(resultado).to be(false), "Fideos.picantes? debería ser false porque deberían tener 2 ajíes. Sin embargo, Fideos.picantes? fue #{resultado}"
+  begin
+    Jor.plato_del_dia = Fideos
+    Jor.picantear!
+    Luchi.suavizar! Fideos, 3
+    resultado = Fideos.picantes?
+    expect(resultado).to be(false), "Fideos.picantes? debería ser false porque deberían tener 2 ajíes. Sin embargo, Fideos.picantes? fue #{resultado}."
+  rescue
+  end
 end
 
 it "Si Jor picantea su plato dos veces y Luchi luego suaviza en 1 ají, los Fideos quedan picantes" do
@@ -51,7 +54,7 @@ it "Luchi descarta la salsa cuando tiene más de 10 ajíes" do
   
   allow(Fideos).to receive(:descartar_la_salsa!)
   Luchi.suavizar! Fideos, 4
-  expect(Fideos).to have_received(:descartar_la_salsa!), "Luchi debería descartar la salsa porque Fideos debería tener 15 ajíes"
+  expect(Fideos).to have_received(:descartar_la_salsa!), "Luchi debería descartar la salsa porque Fideos debería tener 15 ajíes."
 end
 
 it "Luchi no descarta la salsa cuando tiene 10 ajíes" do
@@ -61,7 +64,7 @@ it "Luchi no descarta la salsa cuando tiene 10 ajíes" do
   
   allow(Fideos).to receive(:descartar_la_salsa!)
   Luchi.suavizar! Fideos, 4
-  expect(Fideos).to_not have_received(:descartar_la_salsa!), "Luchi no debería descartar la salsa porque Fideos debería tener 10 ajíes"
+  expect(Fideos).to_not have_received(:descartar_la_salsa!), "Luchi no debería descartar la salsa porque Fideos debería tener 10 ajíes."
 end
 
 it "Luchi no descarta la salsa cuando tiene menos de 10 ajíes" do
@@ -70,5 +73,5 @@ it "Luchi no descarta la salsa cuando tiene menos de 10 ajíes" do
   
   allow(Fideos).to receive(:descartar_la_salsa!)
   Luchi.suavizar! Fideos, 2
-  expect(Fideos).to_not have_received(:descartar_la_salsa!), "Luchi no debería descartar la salsa porque Fideos debería tener 5 ajíes"
+  expect(Fideos).to_not have_received(:descartar_la_salsa!), "Luchi no debería descartar la salsa porque Fideos debería tener 5 ajíes."
 end
