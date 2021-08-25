3fcc17bd7d28274774b9aa396ee53d43f72f9a4f
mayra@mumuki.org <mayra@mumuki.org>
2021-08-25 16:36:48 +0000

    Mumuki Export on 2021-08-25 16:36:48 +0000

--- 00015_Prueba May/test..js
+++ 00015_Prueba May/test..js
@@ -1,5 +1,5 @@
-it(`tieneContraste("claro","claro" retorna false`, function() {
-  let resultado = tieneContraste("claro", "oscuro")
+it(`tieneContraste("claro","oscuro") retorna true`, function() {
+  let resultado = tieneContraste("claro","oscuro")
   assert.equal(resultado)
 })
 
