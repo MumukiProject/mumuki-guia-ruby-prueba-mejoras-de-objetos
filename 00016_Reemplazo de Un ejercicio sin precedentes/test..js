1a20e995f46ae39a979041ae4953adf528aeef65
mayra@mumuki.org <mayra@mumuki.org>
2021-10-01 20:35:47 +0000

    Mumuki Export on 2021-10-01 20:35:47 +0000

--- 00016_Reemplazo de Un ejercicio sin precedentes/test..js
+++ 00016_Reemplazo de Un ejercicio sin precedentes/test..js
@@ -1,5 +1,16 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it(`sePuedeConcentrar("té",100, true) retorna true`, function() {
+  assert(sePuedeConcentrar("té",100, true))
 })
+
+it(`sePuedeConcentrar('mate', 70, true) retorna false`, function() {
+  assert(sePuedeConcentrar('mate', 70, true) )
+})
+
+it(`sePuedeConcentrar('mate', 70, true) retorna false`, function() { 
+  assert(!sePuedeConcentrar('mate', 70, true))
+})
+
+it(`tieneContraste("negro","amarillo") retorna true`, function() {
+  assert(tieneContraste("negro","amarillo"))
+})
+
