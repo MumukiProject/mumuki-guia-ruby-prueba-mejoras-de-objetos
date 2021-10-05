dc6f6835f4d73306760c8743b03c4bd16d02602d
mayra@mumuki.org <mayra@mumuki.org>
2021-10-05 20:26:59 +0000

    Mumuki Export on 2021-10-05 20:26:59 +0000

--- 00018_Reemplazo los paripateticos/test..js
+++ 00018_Reemplazo los paripateticos/test..js
@@ -1,5 +1,19 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it(" Una persona que se desempena en filosofia es de argentina y camina 5kms al dia no es peripatetica", function() {
+  assert.equal((esPeripatetico("filosofía", "Grecia", 5), true);
+})
+
+it("Una persona que se desempena en filosofía es de grecia y camina 2kms al dia no es peripatetica", function() {
+  assert.equal((esPeripatetica("filofofía", "Grecia", 2), false);
+})
+
+it("Una persona que se desempena en atletismo es de Grecia y camina 10kms al dia no es peripatetica", function() {
+  assert.equal(esPeripatetica("atletismo", "Grecia", 10), false);
+})
+
+it("Una persona que se desempena en filosofia es de grecia y camina 3kms al dia es peripatetica", function() {
+  assert.equal(esPeripatetica("atleta", "griego", 10), false);
+})
+
+it("un profesor colombiano que camina 1km al día no es peripatético", function() {
+  assert.equal(esPeripatetico("profesor", "colombiano", 1), false);
 })
