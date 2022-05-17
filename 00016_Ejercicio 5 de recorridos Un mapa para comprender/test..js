aac329916ed40288902726d74be63231f71026a7
mayra@mumuki.org <mayra@mumuki.org>
2022-05-17 17:25:48 +0000

    Mumuki Export on 2022-05-17 17:25:48 +0000

--- 00016_Ejercicio 5 de recorridos Un mapa para comprender/test..js
+++ 00016_Ejercicio 5 de recorridos Un mapa para comprender/test..js
@@ -1,7 +1,8 @@
-it(`Meses de un semestre devuelve seis meses`, function() {
-  assert.deepEqual(meses([{ "mes": "enero", "ganancia": 10 }, { "mes": "febrero", "ganancia": 2 }, { "mes": "marzo", "ganancia": 5 }, { "mes": "abril", "ganancia": 8 }, { "mes": "mayo", "ganancia": 12 }, { "mes": "junio", "ganancia": 25 }]), ["enero", "febrero", "marzo", "abril", "mayo", "junio"]);
-})
-
-it(`Meses de un trimestre devuelve tres meses`, function() {
-  assert.deepEqual([{ "mes": "enero", "ganancia": 10 }, { "mes": "febrero", "ganancia": 2 }, { "mes": "marzo", "ganancia": 5 }]), ["enero", "febrero", "marzo"]);
+describe("", function(){
+  it("meses devuelve los meses de los balances de un trimestre", function() {
+    assert.deepEqual(meses([{ mes: "enero", ganancia: 10 }, { mes: "febrero", ganancia: 2 }, { mes: "marzo", ganancia: 5 }]), ["enero", "febrero", "marzo"])
+  })
+  it("meses devuelve los meses de los balances de un semestre", function() {
+    assert.deepEqual(meses([{ mes: "enero", ganancia: 10 }, { mes: "febrero", ganancia: 2 }, { mes: "marzo", ganancia: 5 }, { mes: "abril", ganancia: 8 }, { mes: "mayo", ganancia: 12 }, { mes: "junio", ganancia: 25 }]), ["enero", "febrero", "marzo", "abril", "mayo", "junio"])
+  })
 })
